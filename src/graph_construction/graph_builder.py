b023e5b8db213c4522b40da31066327f0dbfb4ff
Zayra-Moraes <zayramoraes1212@gmail.com>
2026-06-21 15:45:20 -0300

    feat:add de dfs e save

--- src/graph_construction/graph_builder.py
+++ src/graph_construction/graph_builder.py
@@ -24,9 +24,12 @@ class GraphBuilder:
         self.graph[id2][id1] += 1
         self.graph[id1][id2] += 1
 
-    def add_review(self, words):
+    def add_review(self, words_list):
+        words = list(set(words_list))
         for i in range(len(words)):
             for j in range(i+1, len(words)):
+                if words[i] == words[j]:
+                    continue
                 self.add_edge(words[i], words[j])
 
     def build_graph(self, documents):
@@ -43,7 +46,23 @@ class GraphBuilder:
         return self.id_to_word
 
     def save_graph(self, filename):
-        pass
+        data= {
+            "grafo":{
+                "graph": self.graph,
+                "id_to_word": self.id_to_word,
+                "word_to_id": self.word_to_id,
+            },
+            "dados":{
+                "total_vertices": len(self.id_to_word),
+                "total_arestas": sum(len(v) for v in self.graph.values()) // 2,
+                "numero_componentes": len(self.componentes_conectados()),
+                "componentes_conectados": self.componentes_conectados(),
+                "graus": [self.degree(word) for word in self.id_to_word],
+            }
+        }
+
+        with open(filename, 'w', encoding='utf-8') as f:
+            json.dump(data, f, ensure_ascii=False, indent=4)
 
     # que dados vamos coletar? pq ja tem o page rank
     def bfs(self, start_word):
@@ -65,6 +84,31 @@ class GraphBuilder:
 
         return [self.id_to_word[i] for i in ordem_visita]
 
+    def degree(self, word):
+        if word not in self.word_to_id:
+            return 0
+        return len(self.graph[self.word_to_id[word]])
+
+    def dfs(self, vertice_id, visitados, componente):
+        visitados.add(vertice_id)
+        componente.append(vertice_id)
+        for vizinho_id in self.graph[vertice_id]:
+            if vizinho_id not in visitados:
+                self.dfs(vizinho_id, visitados, componente)
+
+    def componentes_conectados(self):
+        visitados = set()
+        componentes = []
+        for vertice_id in self.graph:
+            if vertice_id not in visitados:
+                componente = []
+                self.dfs(vertice_id, visitados, componente)
+                componente_word = [self.id_to_word[i] for i in componente]
+                componentes.append(componente_word)
+        return componentes
+
+
+
 
 def build_graphs_from_categories(processed_dir, categorias):
 
@@ -82,5 +126,7 @@ def build_graphs_from_categories(processed_dir, categorias):
         total_vertices = len(builder.get_id_to_word())
         total_arestas = sum(len(v) for v in builder.get_graph().values()) // 2
         print(f"{categoria}: {total_vertices} vértices, {total_arestas} arestas")
+        # print("categoria:", categoria)
+        builder.save_graph(f"data/graphs/{categoria}_graph.json")
 
     return builders
